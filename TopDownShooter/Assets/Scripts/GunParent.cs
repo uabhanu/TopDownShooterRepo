4403b013c7d7db0604144acb68c927314d0b8443
Bhanu <uabhanu@gmail.com>
2022-11-12 13:52:35 +0000

    Feature : Player Gun Rotation issue fixed by changing the pivot of the gun sprite to middle centre.

--- TopDownShooter/Assets/Scripts/GunParent.cs
+++ TopDownShooter/Assets/Scripts/GunParent.cs
@@ -26,20 +26,6 @@ public class GunParent : MonoBehaviour
         var aimDirection = (Vector3)aimVector - transform.position;
         var desiredAngle = Mathf.Atan2(aimDirection.y , aimDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.RotateTowards(transform.rotation , Quaternion.Euler(0f , 0f , desiredAngle) , gunDataSo.RotationSpeed * Time.deltaTime);
-
-        var currentZRotationInDegrees = (transform.rotation.z * Mathf.Rad2Deg) * 2; // Z Rotation of the Inspector is Double this value
-        //Debug.Log("Current Z Rotation in Degrees : " + currentZRotationInDegrees);
-
-        //The idea of this is to clamp the rotation between -45 & 45 degrees
-        if(currentZRotationInDegrees < -45f)
-        {
-            //Debug.Log("Set to -45 degrees");
-        }
-
-        if(currentZRotationInDegrees > 45f)
-        {
-            //Debug.Log("Set to 45 degrees");
-        }
     }
 
 
