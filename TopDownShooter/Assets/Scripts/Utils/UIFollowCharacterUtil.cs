b9890e266d3a66ce2399858e0f0605a1043b3fa4
Bhanu <uabhanu@gmail.com>
2023-01-10 22:56:20 +0000

    Features : Added More Colours to the Game

--- TopDownShooter/Assets/Scripts/Utils/UIFollowCharacterUtil.cs
+++ TopDownShooter/Assets/Scripts/Utils/UIFollowCharacterUtil.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 namespace Utils
 {
+    [RequireComponent(typeof(DestroyUtil))]
     public class UIFollowCharacterUtil : MonoBehaviour
     {
+        private DestroyUtil _destroyUtil;
+        
         [SerializeField] private Transform targetTransform;
+
+        private void Awake()
+        {
+            _destroyUtil = GetComponent<DestroyUtil>();
+        }
 
         private void Update()
         {
-            transform.position = targetTransform.position;
+            if(targetTransform != null)
+            {
+                transform.position = targetTransform.position;   
+            }
+            else
+            {
+                _destroyUtil.DestroyObject();
+            }
         }
     }
 }
